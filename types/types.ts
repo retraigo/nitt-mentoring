3b3825fc0f671a3c3a86077caaec50c4f3385849
NeTT <nett@nett.moe>
2023-09-19 19:40:45 +0530

    new schema

--- types/types.ts
+++ types/types.ts
@@ -1,23 +1,51 @@
-export type User = {
-  username: string;
-  id: number;
-  department: number;
-  level: number;
-};
+export type User =
+  & { id: number; username: string; password: string }
+  & ({
+    level: 0;
+    mentee: Mentee;
+  } | {
+    level: 1;
+    mentor: Mentor;
+  } | {
+    level: 2;
+    mentor: Mentor;
+  } | {
+    level: 3;
+    mentor: Mentor;
+  }
+);
 
-export type Mentee = {
-  regno: string;
+export type Department = {
   name: string;
-  year: number;
-  section: string;
-  batch: string;
-  department: number;
-  mentor_id: number;
-};
+  id: string
+}
+
+  export type MentorInfo = {
+    id: number;
+    name: string;
+    department: Department;
+  }
+  export type Mentor = MentorInfo & {
+    id: number;
+    name: string;
+    department: Department;
+    mentees: Mentee[];
+  }
+
+  export type Mentee = {
+    register_number: string;
+    name: string;
+    year: number;
+    section: string;
+    batch: number;
+    department: Department;
+    mentor: MentorInfo
+  }
 
 export type Meeting = {
   id: number;
   date: Date;
   discussion: string;
-  mentee_id: string
+  mentee: Mentee;
+  mentor: MentorInfo;
 };
